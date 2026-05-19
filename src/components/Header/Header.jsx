0a8aeaac65f1b33dc0df60e3a989451e417f917e
Valeria Montoya <github.polygraph767@passmail.net>
2026-05-18 21:17:44 -0600

    style: enhance UI with improved styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,10 @@
+import styles from './Header.module.css';
+
 function Header() {
   return (
     <>
-      <h1>Background color switcher</h1>
-      <p>Click the button to change the background color</p>
+      <h1 class={styles.h1}>Background color switcher</h1>
+      <p class={styles.p}>Click the button to change the background color</p>
     </>
   );
 }
